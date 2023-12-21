3538b0af1b3362ab136a7be1a76aa42e87657249
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2023-12-21 14:42:19 -0500

    coverage

--- Makefile
+++ Makefile
@@ -3,14 +3,40 @@
 BUILDDIR = build
 
 CFLAGS += \
+    -Wall \
+    -Wextra \
+    -Werror \
+    -fstack-protector-strong \
+    -fstack-clash-protection \
+    -fstack-protector-all \
+    -ftrapv \
+    --coverage \
+    -O2 \
+    -g \
     -std=c2x \
 
+ifeq (1,$(ENABLE_ASAN))
+    CFLAGS += \
+        -fsanitize=address \
+        -fsanitize=undefined \
+        -fno-sanitize-recover=all \
+
+endif
+
+# some additional flags if clang is the compiler
+ifneq (,$(findstring clang,$(shell $(CC) --version)))
+    CFLAGS += \
+        -Weverything \
+        -Wno-declaration-after-statement \
+
+endif
+
 $(BUILDDIR)/main: $(BUILDDIR)/main.o $(BUILDDIR)/r36_62.o
-	gcc $(CFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) -o $@ $^
 
 $(BUILDDIR)/%.o: %.c
 	@mkdir -p $(BUILDDIR)
-	gcc $(CFLAGS) -c -o $@ $<
+	$(CC) $(CFLAGS) -c -o $@ $<
 
 .PHONY: test
 test: $(BUILDDIR)/main
@@ -18,9 +44,17 @@ test: $(BUILDDIR)/main
 	cat test.txt | while read line ; do $< $$line | tee -a $(BUILDDIR)/expected.txt; done
 	diff -u0 $(BUILDDIR)/expected.txt expected.txt
 	echo "\n=== Test passed ==="
+# generate lcov report
+	lcov --capture --directory . --no-external --output-file $(BUILDDIR)/coverage.info
+	genhtml $(BUILDDIR)/coverage.info --exclude main.c --output-directory $(BUILDDIR)/coverage
+	echo "\n=== Coverage report ==="
+	echo "See file://$(PWD)/$(BUILDDIR)/coverage/index.html"
 
 .PHONY: snapshot-update
 snapshot-update: $(BUILDDIR)/main
 	rm -f expected.txt
 	cat test.txt | while read line ; do $< $$line | tee -a expected.txt; done
 
+.PHONY: clean
+clean:
+	rm -rf $(BUILDDIR)
--- r36_62.c
+++ r36_62.c
@@ -6,10 +6,10 @@
 // #define DEBUG_(x) x
 #define DEBUG_(x)
 
-const char r62_alphabet[] =
+static const char r62_alphabet[] =
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const char r36_alphabet[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+static const char r36_alphabet[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 #define RADIX_ENCODE(radix_)                           \
   void r##radix_##_encode(char *buf, uint64_t value) { \
@@ -31,28 +31,28 @@ const char r36_alphabet[] = "0123456789abcdefghijklmnopqrstuvwxyz";
     buf[index] = '\0';                                 \
   }
 
-#define RADIX_DECODE(radix_)                                   \
-  uint64_t r##radix_##_alphabet_index(char c) {                \
-    for (uint64_t i = 0; i < radix_; i++) {                    \
-      if (r##radix_##_alphabet[i] == c) {                      \
-        return i;                                              \
-      }                                                        \
-    }                                                          \
-    return 0;                                                  \
-  }                                                            \
-  uint64_t r##radix_##_decode(const char *buf) {               \
-    uint64_t value = 0;                                        \
-    int index = strlen(buf) - 1; /* start at the end */        \
-    uint64_t power = 1;                                        \
-    while (index >= 0) {                                       \
-      DEBUG_(printf("decoding %c\n", buf[index]));             \
-      uint64_t alpha = r##radix_##_alphabet_index(buf[index]); \
-      DEBUG_(printf("alpha: %lu\n", alpha));                   \
-      value += r##radix_##_alphabet_index(buf[index]) * power; \
-      power *= radix_;                                         \
-      index--;                                                 \
-    }                                                          \
-    return value;                                              \
+#define RADIX_DECODE(radix_)                                          \
+  static uint64_t r##radix_##_alphabet_index(char c) {                \
+    for (uint64_t i = 0; i < radix_; i++) {                           \
+      if (r##radix_##_alphabet[i] == c) {                             \
+        return i;                                                     \
+      }                                                               \
+    }                                                                 \
+    return 0;                                                         \
+  }                                                                   \
+  uint64_t r##radix_##_decode(const char *buf) {                      \
+    uint64_t value = 0;                                               \
+    int index = (int)strlen(buf) - 1; /* start at the end */          \
+    uint64_t power = 1;                                               \
+    while (index >= 0) {                                              \
+      DEBUG_(printf("decoding %c\n", buf[index]));                    \
+      DEBUG_(uint64_t alpha = r##radix_##_alphabet_index(buf[index]); \
+             printf("alpha: %lu\n", alpha));                          \
+      value += r##radix_##_alphabet_index(buf[index]) * power;        \
+      power *= radix_;                                                \
+      index--;                                                        \
+    }                                                                 \
+    return value;                                                     \
   }
 
 RADIX_ENCODE(62)
