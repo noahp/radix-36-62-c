2115228cd9775c50f5a8f1f70a6eb6d0fc9096cf
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2023-12-21 14:43:29 -0500

    update

--- example.c
+++ example.c
@@ -6,10 +6,12 @@ int main() {
   char buf[14];
 
   r36_encode(buf, 4294967295);
-  printf("%s\n", buf);  // 3z141z1
+  printf("%s\n", buf);  // 1z141z3
+
+  printf("%lu\n", r36_decode(buf));  // 4294967295
 
   r62_encode(buf, 4294967295);
-  printf("%s\n", buf);  // 3cfFG4
+  printf("%s\n", buf);  // 4GFfc3
 
   printf("%lu\n", r62_decode(buf));  // 4294967295
 
